97c7696c3bf5efdc70aee25907651008278f0fea
haddar <medhedi.haddar@gmail.com>
2017-12-02 15:34:07 +0100

    Merge branch 'master' of https://github.com/therbel/chess

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 
+		let joueur="white";
 	$(window).ready(function(){
-		let quiJoue="white";
+	
+
+			if (joueur=="white") {
+				$( function() {
+		$( ".black" ).draggable( "disable" );
+		$( ".white" ).draggable( "enable" );			
+				});
+		
+	}
+	else
+	{
+			$( function() {
+		$( ".white" ).draggable( "disable" );
+		$( ".black" ).draggable( "enable" );
 	});
+	} 
+	});
+
 
 
 $( function() {
@@ -11,13 +28,12 @@ $( function() {
     		let source=document.getElementById(ui.helper.attr('id')).parentNode;
     		
     		findMove(ui.helper.attr('id'));
+    		console.log(ui.helper.attr('class'));
     	},
     	stop: function( event, ui ) {
     		console.log('out');
     	}
   });
-
-   
 });
 
 function findMove(pieceId){
@@ -30,15 +46,12 @@ function findMove(pieceId){
 	// type de piece
 		let typeOfPiece=(pieceId.split('-'))[0];
 		let joueur=(pieceId.split('-'))[1];
-		console.log(parentCaseL+" // "+parentCaseC);
 		console.log(joueur);
 
 if(joueur=='black'){
 		switch(typeOfPiece){
 			case 'pion':
-
-				// chercher les cases possible à y dropper le pion  
-				// $("#"+(parseInt(parentCaseL,10)+1)+'-'+parentCaseC).css("border-color","red");							
+				// chercher les cases possible à y dropper le pion  						
 					if($("#"+(parseInt(parentCaseL,10)+1)+'-'+parentCaseC).html()==""){
 
 				$("#"+(parseInt(parentCaseL,10)+1)+'-'+parentCaseC).droppable({
@@ -49,8 +62,6 @@ if(joueur=='black'){
 		        	draggableItem.style.left="";
 		    			ui.draggable.detach().appendTo($(this));
 		    			initialize();
-	    			 	// $(this).css("border-color","transparent");
-      		
       			}
 				});
 				}
@@ -61,24 +72,25 @@ if(joueur=='black'){
 if(joueur=='white'){
 		switch(typeOfPiece){
 			case 'pion':
-				if($("#"+(parseInt(parentCaseL,10)-1)+'-'+parentCaseC).html()==""){
-
+				if($("#"+(parseInt(parentCaseL,10)-1)+'-'+parentCaseC).html()==""){				
 				$("#"+(parseInt(parentCaseL,10)-1)+'-'+parentCaseC).droppable({
 						accept:"#"+pieceId,
 						drop: function( event, ui ) {
 		      	  let draggableItem=document.getElementById(ui.draggable.attr("id"))
 		    	    draggableItem.style.top="";
 		        	draggableItem.style.left="";
-
 		    			ui.draggable.detach().appendTo($(this));
 		    			initialize();
+		    			joueur="black";
       			}
 				});
 				}
 				else if('hello'){
 
 				}								
+				attaque("white","pion",[parentCaseL,parentCaseC],pieceId);
 		}
+
 }	
 }
 
@@ -91,13 +103,76 @@ function initialize(){
 
 }
 
-function attaque(joueur,type,position){
-	let l=position[0];
-	let c=position[1];
+function attaque(joueur,type,position,pieceId){
+	let l=parseInt(position[0],10);
+	let c=parseInt(position[1],10);
 	
 	if(joueur=="white"){
+
 		if (type=="pion") {
-// code ...
+		
+			if( ($("#"+(l-1)+"-"+(c-1)).children().attr('id'))!=undefined){
+
+					let e=$("#"+(l-1)+"-"+(c-1)).children().attr('id');					
+		
+						if(e.split("-")[1]=="black"){
+
+
+								console.log('gauche');
+								$("#"+(l-1)+"-"+(c-1)).droppable({
+											accept:'#'+pieceId,
+											drop: function( event, ui ) {
+							      	  let draggableItem=document.getElementById(ui.draggable.attr("id"));
+							    	    draggableItem.style.top="";
+							        	draggableItem.style.left="";
+							    			ui.draggable.detach().appendTo($(this));
+							    			console.log($(this).find("img:first-child"));
+							    			$("#tresorWhite").append($(this).find("img:first-child"));
+							    			// initialiser les case droppable;
+							    			initialize();						      		
+					      			}
+									});
+
+						}
+			}
+			if( ($("#"+(l-1)+"-"+(c+1)).children().attr('id'))!=undefined){
+
+					let e=$("#"+(l-1)+"-"+(c+1)).children().attr('id');					
+		
+						if(e.split("-")[1]=="black"){
+
+								console.log('droite');
+								$("#"+(l-1)+"-"+(c+1)).droppable({
+											accept:'#'+pieceId,
+											drop: function( event, ui ) {
+							      	  let draggableItem=document.getElementById(ui.draggable.attr("id"));
+							    	    draggableItem.style.top="";
+							        	draggableItem.style.left="";
+							    			ui.draggable.detach().appendTo($(this));
+							    			console.log($(this).find("img:first-child"));
+							    			$("#tresorWhite").append($(this).find("img:first-child"));
+							    			// initialiser les case droppable;
+							    			initialize();					      		
+					      			}
+									});
+
+						}
+			}
+		
+				// if(document.getElementById((l-1)+"-"+(c-1)))
 		}
+	}
+}
+
+
+function quiJou(){
+	if (joueur=="white") {
+		$( ".black" ).droppable( "disable" );
+		$( ".white" ).droppable( "enable" );
+	}
+	else
+	{
+		$( ".white" ).droppable( "disable" );
+		$( ".black" ).droppable( "enable" );
 	}
 }
